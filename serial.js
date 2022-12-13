8bf0b6affe8fde5dd2ebfd5d461bf4e94445b8e6
MAC <mac@Joseph-2.local>
2022-12-13 09:51:09 +0800

    12/13 9:50

--- serial.js
+++ serial.js
@@ -6,6 +6,7 @@ const db = getDatabase();//firebase
 const connect_router = ref(db,'/')//firebase
 
 const arduinoPort = 'COM4'
+const _arduinoPort = ''
 const port = new SerialPort(arduinoPort, { bauRate: 9600 }, (err) => {
     if (err) {
         console.log('fail');
@@ -20,7 +21,7 @@ function show_num() {
         port.on("data", function (d) {
             let real_data = parseFloat(d)
             if (real_data) { // 拉下拉霸
-                // console.log("當前數值：" + real_data)
+                //console.log("當前數值：" + real_data)
                 let data = real_data/10
                 update(connect_router, {
                     'num':data,
